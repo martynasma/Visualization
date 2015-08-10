bc4546253a294568de3052a67096426c30e15c01
Mathew Zummo <mzummo@gmail.com>
2015-08-10 10:57:41 -0400

    changes
    Signed-off-by: Mathew Zummo <mzummo@gmail.com>

--- test/util.js
+++ test/util.js
@@ -1,34 +1,31 @@
 "use strict";
 define(["src/common/Utility"], function (Utility) {
-    describe("util", function () {
+    describe("utility", function () {
 
-        var path = "src/common/Utility";
+        it("util-naturalSort-test", function (done) {
 
-        require([path], function (Util) {
-            it("util-naturalSort-test", function (done) {
+            var list = ["a","b","c","AA","AB","AB123","CD456",123,456,"111","222"];
 
-                var list = ["a","b","c","AA","AB","AB123","CD456",123,456,"111","222"];
+            var sortedListA = Utility.naturalSort(list,"ascending",null,false);
+            var expectedListA = ["111", 123, "222", 456, "a", "AA", "AB", "AB123", "b", "c", "CD456"];
 
-                var sortedListA = Util.naturalSort(list,"ascending",null,false);
-                var expectedListA = ["111", 123, "222", 456, "a", "AA", "AB", "AB123", "b", "c", "CD456"];
+            var sortedListB = Utility.naturalSort(list,"descending",null,false);
+            var expectedListB = [456, "222", 123, "111", "CD456", "c", "b", "AB123", "AB", "AA", "a"];
 
-                var sortedListB = Util.naturalSort(list,"descending",null,false);
-                var expectedListB = [456, "222", 123, "111", "CD456", "c", "b", "AB123", "AB", "AA", "a"];
+            var sortedListC = Utility.naturalSort(list,"ascending",null,true);
+            var expectedListC = ["111", 123, "222", 456, "AA", "AB", "AB123", "CD456", "a", "b", "c"];
 
-                var sortedListC = Util.naturalSort(list,"ascending",null,true);
-                var expectedListC = ["111", 123, "222", 456, "AA", "AB", "AB123", "CD456", "a", "b", "c"];
+            var sortedListD = Utility.naturalSort(list,"descending",null,true);
+            var expectedListD = [456, "222", 123, "111", "c", "b", "a", "CD456", "AB123", "AB", "AA"];
 
-                var sortedListD = Util.naturalSort(list,"descending",null,true);
-                var expectedListD = [456, "222", 123, "111", "c", "b", "a", "CD456", "AB123", "AB", "AA"];
+            assert.deepEqual(sortedListA,expectedListA);
+            assert.deepEqual(sortedListB,expectedListB);
+            assert.deepEqual(sortedListC,expectedListC);
+            assert.deepEqual(sortedListD,expectedListD);
 
-                assert.deepEqual(sortedListA,expectedListA);
-                assert.deepEqual(sortedListB,expectedListB);
-                assert.deepEqual(sortedListC,expectedListC);
-                assert.deepEqual(sortedListD,expectedListD);
-
-                done();
-            });
+            done();
         });
+
     });
 });
 
